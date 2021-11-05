248369c3f17a67a3e8ab12347a2d73e7c310a3f3
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-11-05 21:54:56 +0000

    Adjusted so body covers all, and mainContent is restricted instead

--- navBar.js
+++ navBar.js
@@ -1,5 +1,5 @@
 document.write(`
 	<ul class="navbar">
-		<li class="navbarItem"><a href="/bs-api-docs/">(Unofficial) BS Api Docs</a></li>
+		<li><a href="/bs-api-docs/">(Unofficial) BS Api Docs</a></li>
 	</ul>
 `);
